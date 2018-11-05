504ebf555b275103b0c97d7cedf83f6fe5982f76
thingsturn <wangchong@thingsturn.com>
2018-11-05 11:12:04 +0800

    删除无用文件，修改编译脚本

--- src/make_src_a.sh
+++ src/make_src_a.sh
@@ -8,7 +8,7 @@ for dir in app network os; do
     cd $dir
 	make clean
 	make COMPILE=gcc APP_DEBUG=0 DEBUG=0
-	echo "move lib$dir.lib to lib folder"
+	echo "move lib$dir.a to lib folder"
 	cp .output/w600/lib/lib$dir.a ../../lib/lib$dir.a
     cd ..
 done;
